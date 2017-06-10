f58fee3f0bfe7422919e5621eba85525e0a8ac43
purelind <kevinsocial@outlook.com>
2017-06-10 21:42:18 +0800

    tab setup.py-1.1.0

--- nester/nester.py
+++ nester/nester.py
@@ -1,10 +1,12 @@
-def print_lol(the_list):
+def print_lol(the_list, level):
     """
     para the_list: type list
     return : void
         """
     for each_item in the_list:
         if isinstance(each_item, list):
-            print_lol(each_item)
+            print_lol(each_item, level+1)
         else:
+            for tab_stop in range(level):
+                print("\t", end="")
             print(each_item)
