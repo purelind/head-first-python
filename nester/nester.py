fde6e239e8b66a917814a6afa467552d3f0911c6
purelind <kevinsocial@outlook.com>
2017-06-10 22:08:05 +0800

    add third argumet: indent. setup.py 1.3.0

--- nester/nester.py
+++ nester/nester.py
@@ -1,12 +1,13 @@
-def print_lol(the_list, level=0):
+def print_lol(the_list, indent=False, level=0):
     """
     para the_list: type list
     return : void
         """
     for each_item in the_list:
         if isinstance(each_item, list):
-            print_lol(each_item, level+1)
+            print_lol(each_item, indent, level+1)
         else:
-            for tab_stop in range(level):
-                print("\t", end="")
+            if indent:
+                for tab_stop in range(level):
+                    print("\t", end="")
             print(each_item)
