0dafbbd868ea2a140c2e8fea1bb3f53e4eb6350d
purelind <kevinsocial@outlook.com>
2017-06-10 20:43:44 +0800

    pypy nester-kevin-test

--- nester/setup.py
+++ nester/setup.py
@@ -1,10 +1,10 @@
 from distutils.core import setup
 
 setup(
-        name          = 'nester',
+        name          = 'nester-kevin-test',
         version       = '1.0.0',
         py_modules    = ['nester'],
-        author        = 'kevinpython',
+        author        = 'kevin',
         author_email  = 'kevinsocial@outlook.com',
         url           = 'http://www.headfirstlabs.com',
         description   = 'A simple printer of nested lists',
