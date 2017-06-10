849b5d5fbf72ecb138e06ce6b19c48a05c8d73be
purelind <kevinsocial@outlook.com>
2017-06-11 07:53:56 +0800

    add split extra argument

--- chapter3/sketch.py
+++ chapter3/sketch.py
@@ -2,7 +2,7 @@ import os
 
 data = open('sketch.txt')
 for each_line in data:
-    (role,  line_spoken) = each_line.split(':')
+    (role,  line_spoken) = each_line.split(':', 1)
     print(role, end='')
     print(' said: ', end='')
     print(line_spoken, end='')
