d853f2d964c15b79af62d2e678a32e669b147759
purelind <kevinsocial@outlook.com>
2017-06-10 19:54:00 +0800

    add dir nester/

---
+++ nester/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name          = 'nester',
+        version       = '1.0.0',
+        py_modules    = ['nester'],
+        author        = 'kevinpython',
+        author_email  = 'kevinsocial@outlook.com',
+        url           = 'http://www.headfirstlabs.com',
+        description   = 'A simple printer of nested lists',
+)
