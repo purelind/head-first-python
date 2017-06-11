b02aaad8e6593003259eb65b8f94f36c55f988fc
purelind <kevinsocial@outlook.com>
2017-06-11 08:27:52 +0800

    add exception

--- chapter3/sketch.py
+++ chapter3/sketch.py
@@ -3,11 +3,13 @@ import os
 data = open('sketch.txt')
 
 for each_line in data:
-    if not each_line.find(":") == -1:
+    try:
         (role,  line_spoken) = each_line.split(':', 1)
         print(role, end='')
         print(' said: ', end='')
         print(line_spoken, end='')
+    except:
+        pass
 
 data.close()
 
