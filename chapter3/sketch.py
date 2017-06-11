24003810f4ea36c4afcd8242522476cd930e6ec8
purelind <kevinsocial@outlook.com>
2017-06-11 08:19:28 +0800

    add data.close(); add extra logic to deal with line not including ':'

--- chapter3/sketch.py
+++ chapter3/sketch.py
@@ -1,10 +1,13 @@
 import os
 
 data = open('sketch.txt')
-for each_line in data:
-    (role,  line_spoken) = each_line.split(':', 1)
-    print(role, end='')
-    print(' said: ', end='')
-    print(line_spoken, end='')
 
+for each_line in data:
+    if not each_line.find(":") == -1:
+        (role,  line_spoken) = each_line.split(':', 1)
+        print(role, end='')
+        print(' said: ', end='')
+        print(line_spoken, end='')
+
+data.close()
 
