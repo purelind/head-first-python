578a0deb911a70da34101070d09f84e9fc76eb7a
purelind <kevinsocial@outlook.com>
2017-06-11 18:10:03 +0800

    change print to nester.print_lol to make data display in vivid way

--- chapter3/sketchStore.py
+++ chapter3/sketchStore.py
@@ -1,3 +1,5 @@
+from nester import print_lol
+
 man = []
 other = []
 
@@ -21,8 +23,8 @@ except IOError:
 
 try:
     with open('man_data.txt', 'w') as man_file, open('other_data.txt', 'w') as other_file:
-        print(man, file=man_file)
-        print(other, file=other_file)
+        print_lol(man, fh=man_file)
+        print_lol(other, fh=other_file)
 
 except IOError as err:
     print('File error: ' + str(err))
