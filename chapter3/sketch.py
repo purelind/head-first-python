f2b1c5da4a98933beb83015f12ee28bf787f5bf1
purelind <kevinsocial@outlook.com>
2017-06-11 08:34:58 +0800

    add extra error check

--- chapter3/sketch.py
+++ chapter3/sketch.py
@@ -1,15 +1,18 @@
 import os
 
-data = open('sketch.txt')
+if os.path.exists('sketch.txt'):
+    data = open('sketch.txt')
 
-for each_line in data:
-    try:
-        (role,  line_spoken) = each_line.split(':', 1)
-        print(role, end='')
-        print(' said: ', end='')
-        print(line_spoken, end='')
-    except:
-        pass
+    for each_line in data:
+        try:
+            (role,  line_spoken) = each_line.split(':', 1)
+            print(role, end='')
+            print(' said: ', end='')
+            print(line_spoken, end='')
+        except:
+            pass
 
-data.close()
+    data.close()
+else:
+    print('The data file is missing!')
 
