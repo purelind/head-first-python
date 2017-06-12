06183d1751dcfd2cc3b0b39c58649ce95e994d23
purelind <kevinsocial@outlook.com>
2017-06-12 09:24:13 +0800

    def a function to simplify 'with open'

--- chapter5/timeRecord.py
+++ chapter5/timeRecord.py
@@ -1,45 +1,26 @@
 from sanitize import sanitize
 
-# remove null and ','
-with open('james.txt') as jaf:
-    data = jaf.readline()
-james = data.strip().split(',')
 
-with open('julie.txt') as juf:
-    data = juf.readline()
-julie = data.strip().split(',')
+def get_coach_data(filename):
+    """
 
-with open('mikey.txt') as mif:
-    data = mif.readline()
-mikey= data.strip().split(',')
+    :param filename: eg: 'james.txt'
+    :return: list which has removed null and ','
+    """
+    try:
+        with open(filename) as f:
+            data = f.readline()
+        return data.strip().split(',')
+    except IOError as ioerr:
+        print('File error: ' + str(ioerr))
+        return None
 
-with open('sarah.txt') as saf:
-    data = saf.readline()
-    sarah = data.strip().split(',')
+james = get_coach_data('james.txt')
+julie = get_coach_data('julie.txt')
+mikey = get_coach_data('mikey.txt')
+sarah = get_coach_data('sarah.txt')
 
-james = sorted([sanitize(t) for t in james])
-julie = sorted([sanitize(t) for t in julie])
-mikey = sorted([sanitize(t) for t in mikey])
-sarah = sorted([sanitize(t) for t in sarah])
-
-unique_james = []
-unique_julie = []
-unique_mikey = []
-unique_sarah = []
-
-for each_t in james:
-    if each_t not in unique_james:
-        unique_james.append(each_t)
-for each_t in julie:
-    if each_t not in unique_julie:
-        unique_julie.append(each_t)
-for each_t in mikey:
-    if each_t not in unique_mikey:
-        unique_mikey.append(each_t)
-for each_t in sarah:
-    if each_t not in unique_sarah:
-        unique_sarah.append(each_t)
-
-print(unique_james[0:3])
-print(unique_julie[0:3])
-print(unique_mikey[0:3])
+print(sorted(set([sanitize(t) for t in james]))[0:3])
+print(sorted(set([sanitize(t) for t in julie]))[0:3])
+print(sorted(set([sanitize(t) for t in mikey]))[0:3])
+print(sorted(set([sanitize(t) for t in sarah]))[0:3])
