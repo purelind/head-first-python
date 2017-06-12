61ce511959241bdf426eceaca4c11967acfab55d
purelind <kevinsocial@outlook.com>
2017-06-12 13:56:23 +0800

    Ahtlete.py --> ahtletelist.py

--- chapter6/athletelist.py
+++ chapter6/athletelist.py
@@ -1,17 +1,17 @@
-class Athlete:
-    def __init__(self, a_name, a_dob=None, a_times=[]):
-        self.name = a_name
-        self.dob = a_dob
-        self.times = a_times
-
-    def top3(self):
-        return sorted(set([sanitize(t) for t in self.times]))[0:3]
-
-    def add_time(self, time_value):
-        self.times.append(time_value)
-
-    def add_times(self, list_of_times):
-        self.times.extend(list_of_times)
+# class Athlete:
+#     def __init__(self, a_name, a_dob=None, a_times=[]):
+#         self.name = a_name
+#         self.dob = a_dob
+#         self.times = a_times
+#
+#     def top3(self):
+#         return sorted(set([sanitize(t) for t in self.times]))[0:3]
+#
+#     def add_time(self, time_value):
+#         self.times.append(time_value)
+#
+#     def add_times(self, list_of_times):
+#         self.times.extend(list_of_times)
 
 
 def sanitize(time_string):
@@ -22,7 +22,18 @@ def sanitize(time_string):
     else:
         return(time_string)
     (mins, secs) = time_string.split(splitter)
-    return(mins + '.' + secs)
+    return mins + '.' + secs
+
+
+class AthleteList(list):
+    def __init__(self, a_name, a_dob=None, a_times=[]):
+        list.__init__([])
+        self.name = a_name
+        self.dob = a_dob
+        self.extend(a_times)
+
+    def top3(self):
+        return sorted(set([sanitize(t) for t in self]))[0:3]
 
 
 def get_coach_data(filename):
@@ -30,10 +41,10 @@ def get_coach_data(filename):
         with open(filename) as f:
             data = f.readline()
         templ = data.strip().split(',')
-        return Athlete(templ.pop(0), templ.pop(0), templ)
+        return AthleteList(templ.pop(0), templ.pop(0), templ)
     except IOError as ioerr:
         print('File error: ' + str(ioerr))
-        return(None)
+        return None
 
 james = get_coach_data('james2.txt')
 julie = get_coach_data('julie2.txt')
