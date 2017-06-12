f2f9fb077ca962e340fd52fc8d28c72cf8894d35
purelind <kevinsocial@outlook.com>
2017-06-12 08:46:43 +0800

    remove duplicate; display three the smallest time datas

--- chapter5/timeRecord.py
+++ chapter5/timeRecord.py
@@ -17,10 +17,29 @@ with open('sarah.txt') as saf:
     data = saf.readline()
     sarah = data.strip().split(',')
 
+james = sorted([sanitize(t) for t in james])
+julie = sorted([sanitize(t) for t in julie])
+mikey = sorted([sanitize(t) for t in mikey])
+sarah = sorted([sanitize(t) for t in james])
 
-print(sorted([sanitize(t) for t in james]))
-print(sorted([sanitize(t) for t in julie]))
-print(sorted([sanitize(t) for t in mikey]))
-print(sorted([sanitize(t) for t in sarah]))
+unique_james = []
+unique_julie = []
+unique_mikey = []
+unique_sarah = []
 
+for each_t in james:
+    if each_t not in unique_james:
+        unique_james.append(each_t)
+for each_t in julie:
+    if each_t not in unique_julie:
+        unique_julie.append(each_t)
+for each_t in mikey:
+    if each_t not in unique_mikey:
+        unique_mikey.append(each_t)
+for each_t in sarah:
+    if each_t not in unique_sarah:
+        unique_sarah.append(each_t)
 
+print(unique_james[0:3])
+print(unique_julie[0:3])
+print(unique_mikey[0:3])
