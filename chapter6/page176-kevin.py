4ba994cb22d8ed20361febc9992ed5ea44ca3cc6
purelind <kevinsocial@outlook.com>
2017-06-12 11:10:20 +0800

    get_coach_data() return dict directly

--- chapter6/page176-kevin.py
+++ chapter6/page176-kevin.py
@@ -13,16 +13,21 @@ def get_coach_data(filename):
     try:
         with open(filename) as f:
             data = f.readline()
-        return data.strip().split(',')
+        templ = data.strip().split(',')
+        return {'Name': templ.pop(0),
+                'DOB': templ.pop(0),
+                'Times': str(sorted(set([sanitize(t) for t in templ]))[0:3])}
     except IOError as ioerr:
         print('File error: ' + str(ioerr))
         return None
 
+james = get_coach_data('james2.txt')
+julie = get_coach_data('julie2.txt')
+mikey = get_coach_data('mikey2.txt')
 sarah = get_coach_data('sarah2.txt')
+print(james['Name'] + "'s fastest times are: " + james['Times'])
+print(julie['Name'] + "'s fastest times are: " + julie['Times'])
+print(mikey['Name'] + "'s fastest times are: " + mikey['Times'])
+print(sarah['Name'] + "'s fastest times are: " + sarah['Times'])
 
-sarah_data = {}
-sarah_data['Name'] = sarah.pop(0)
-sarah_data['DOB'] = sarah.pop(0)
-sarah_data['Times'] = sarah
-print(sarah_data['Name'] + "'s fastest times are: " +
-      str(sorted(set([sanitize(t) for t in sarah_data['Times']]))[0:3]))
+
